e1719434d5ed925bd763be02c133d2f350fbed21
infographicstw <contact@infographics.tw>
2015-05-02 09:52:42 +0800

    move slightly to left in case legend out of bound

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 d3.json("quake.json", function(quake) {
 d3.json("world.json", function(world) {
 
-var projection = d3.geo.orthographic().scale(225).translate([400,300]).clipAngle(90);
+var projection = d3.geo.orthographic().scale(225).translate([300,300]).clipAngle(90);
 var path = d3.geo.path().projection(projection);
 var pathQuake = d3.geo.path().projection(projection).pointRadius(function(it) { return magmap(parseFloat(it.properties.mag)); });
 var countries = topojson.feature(world, world.objects.countries).features;
@@ -33,14 +33,14 @@ var updateQuakeLocation = function() {
 // legend
 magtick = [2,3,4,5,6,7,8];
 d3.select("#svg").selectAll("circle").data(magtick).enter().append("circle").attr({
-  cx: function(it,idx) { return idx * 10 + magmap(it) + 600; },
+  cx: function(it,idx) { return idx * 10 + magmap(it) + 500; },
   cy: function(it,idx) { return 500; },
   r: function(it,idx) { return magmap(it); },
   fill: "none",
   stroke: "#f00"
 });
 d3.select("#svg").selectAll("text").data(magtick).enter().append("text").attr({
-  x: function(it,idx) { return idx * 10 + magmap(it) + 600; },
+  x: function(it,idx) { return idx * 10 + magmap(it) + 500; },
   y: function(it,idx) { return 530; }
 }).style({
   "text-anchor": "middle"
